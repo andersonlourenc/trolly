ec3ffe09790cf632cf4f1ce2e7e73ff5375ef6ed
Anderson Lourenço <andersonldev@gmail.com>
2025-04-18 10:56:21 -0300

    added login manual

--- app/src/main/java/com/lourenc/trolly/MainActivity.kt
+++ app/src/main/java/com/lourenc/trolly/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.lourenc.trolly.splash.SplashScreen
+import com.lourenc.trolly.ui.login.LoginScreen
+
 import com.lourenc.trolly.ui.theme.TrollyTheme
+
 
 
 class MainActivity : ComponentActivity() {
@@ -14,17 +21,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             setContent {
+                val navController: NavHostController = rememberNavController()
+
                 TrollyTheme {
-                    SplashScreen(
-                        onLoginClick = { /* TODO: Navegar para tela de login */ },
-                        onRegisterClick = { /* TODO: Navegar para tela de registro */ }
-                    )
+                     NavHost(
+                         navController = navController,
+                         startDestination = "splash"
+                     ) {
+                        composable("splash") {
+                            SplashScreen(
+                                onLoginClick = { navController.navigate("login") },
+                                onRegisterClick = { navController.navigate("register")}
+                                    )
+                                }
+                         composable("login") {
+                             LoginScreen(
+                                 println("Login feito")
+                             )
+                         }
+                     }
+
                 }
             }
 
         }
 
     }
-
 
 }
