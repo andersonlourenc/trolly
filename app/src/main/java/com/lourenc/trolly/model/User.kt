9975b17b7db594b0ac00b3f99a049c20bf29288c
Anderson Lourenço <andersonldev@gmail.com>
2025-04-18 22:50:17 -0300

    update project

--- app/src/main/java/com/lourenc/trolly/model/User.kt
+++ app/src/main/java/com/lourenc/trolly/model/User.kt
@@ -6,3 +6,15 @@ data class User(
     val email: String,
     val password: String
 )
+
+object FakeUserDatabase {
+    val users = mutableListOf<User>()
+
+    fun emailExists(email: String): Boolean {
+        return users.any { it.email == email }
+    }
+
+    fun addUser(user: User) {
+        users.add(user)
+    }
+}
