3b8e4519cd5b9ce8131d1ae4f9e9465909c764af
Anderson Lourenço <andersonldev@gmail.com>
2025-05-16 23:06:25 -0300

    top bar home and login adjustments

--- app/src/main/java/com/lourenc/trolly/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/lourenc/trolly/ui/screens/HomeScreen.kt
@@ -1,55 +1,85 @@
 package com.lourenc.trolly.ui.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.icons.filled.Construction
+import com.lourenc.trolly.R
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(24.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Icon(
-                imageVector = Icons.Default.Construction,
-                contentDescription = "Em construção",
-                modifier = Modifier.size(80.dp),
-                tint = Color.Gray
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Estamos construindo algo incrível! 💪",
-                style = MaterialTheme.typography.titleMedium,
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Volte em breve!",
-                style = MaterialTheme.typography.bodyMedium,
-                color = Color.Gray
-            )
+
+    Scaffold(
+        topBar = {
+
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.backgroundtitlebar),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(128.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text("Home", style = MaterialTheme.typography.titleMedium)
+                    },
+                    actions = {
+                        IconButton(onClick = {
+
+
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Notifications, // ícone de sino
+                                contentDescription = "Notificações"
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                        titleContentColor = MaterialTheme.colorScheme.onSurface,
+                        actionIconContentColor = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+            }
+        },
+        content = { innerPadding ->
+
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(16.dp) //
+            ) {
+
+
+            }
         }
-    }
+    )
+
+
 }
+
+
+
